b5dffb12c89d43173e8ed055e6538abf7473d728
ZachJRichman <zjrichman@outlook.com>
2024-10-11 14:57:39 -0400

    added gradle

---
+++ lit-template-1.21.1/src/main/java/package/lit/LitDataGenerator.java
@@ -0,0 +1,11 @@
+package package.lit;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class LitDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
